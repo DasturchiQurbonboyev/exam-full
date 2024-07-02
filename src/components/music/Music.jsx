a86ccdea3ee93e8d93d07d367ef33169f7b52bf1
DasturchiQurbonboyev <dasturchiqurbonboyev@gmail.com>
2024-07-02 23:59:46 +0500

    dcsdcsdcs

--- src/components/music/Music.jsx
+++ src/components/music/Music.jsx
@@ -14,11 +14,11 @@ const Music = () => {
     }, [])
 
     return (
-        <div data-aos="fade-up" className='kontainer rounded-md bg-black items-center grid grid-cols-2'>
-            <div className='flex flex-col justify-center gap-10 pt-10 pl-10 pb-10'>
+        <div data-aos="fade-up" className='kontainer rounded-md bg-black items-center grid min-[750px]:grid-cols-2'>
+            <div className='flex flex-col justify-center gap-10 pt-10  min-[500px]:pl-10 pb-10'>
                 <p data-aos="fade-up" className='text-[#00FF66]    '>Categories</p>
-                <h2 data-aos="fade-up" className=' text-[#FAFAFA] text-[48px] font-[600]   '  >Enhance Your Music Experience</h2>
-                <div className='flex gap-[24px]'>
+                <h2 data-aos="fade-up" className=' text-[#FAFAFA] text-[25px] min-[500px]:text-[30px] min-[910px]:text-[48px] font-[600]   '  >Enhance Your Music Experience</h2>
+                <div className='flex max-[500px]:flex-wrap gap-[24px]'>
                     <div data-aos="fade-up" className='w-[62px] h-[62px] bg-white flex flex-col items-center rounded-[50%] justify-center    '>
                         <h5 className='font-[700] leading-[20px]'>25</h5>
                         <p>Hours</p>
